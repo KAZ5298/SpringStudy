69e4819e8463ea0f18c109d9b634a7e848e29dd0
KAZUHIKO <kazuhiko.d@gmail.com>
2024-07-11 11:40:42 +0900

    11.2.1 直リンクの禁止 完了

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity   // このアノテーションはこのアプリではなくてよい
+public class SecurityConfig {
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                .requestMatchers(mvc.pattern("/user/signup")).permitAll()
+                .anyRequest().authenticated()
+        );
+
+        http.headers(headers -> headers
+                .frameOptions(FrameOptionsConfig::disable)
+        );
+
+        // CSRF 対策を無効に設定 (一時的)
+        http.csrf(csrf -> csrf
+                .ignoringRequestMatchers(PathRequest.toH2Console())
+                .disable()
+        );
+
+        return http.build();
+    }
+}
